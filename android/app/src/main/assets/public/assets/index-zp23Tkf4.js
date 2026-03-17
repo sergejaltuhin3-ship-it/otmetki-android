48ac146eb7a5e1088d1cf7da53b48594a074b7d3
sergejaltuhin3-ship-it <sergejaltuhin3@gmail.com>
2026-03-17 20:28:34 +0500

    Add android/app/src/main/assets/public/assets/index-zp23Tkf4.js

---
+++ android/app/src/main/assets/public/assets/index-zp23Tkf4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-EDSomB6w.js","assets/index-CKEmiHoO.js","assets/index-BfrArCu3.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CKEmiHoO.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-EDSomB6w.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
