f9f3de488818da98d5e7d3af68c1d1aacf97e075
sergejaltuhin3-ship-it <sergejaltuhin3@gmail.com>
2026-03-17 20:28:31 +0500

    Add android/app/src/main/assets/public/assets/index-DfeapWUO.js

---
+++ android/app/src/main/assets/public/assets/index-DfeapWUO.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Dsfcwfuq.js","assets/index-CKEmiHoO.js","assets/index-BfrArCu3.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-CKEmiHoO.js";const _=p("App",{web:()=>r(()=>import("./web-Dsfcwfuq.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
